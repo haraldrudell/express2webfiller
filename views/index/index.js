1c12bc900996f33ecc425cc6f03e7063e1b076e6
Harald Rudell <harald@therudells.com>
2012-08-24 21:50:55 -0700

    0.0.2 Works wih webfiller 0.0.4 and tagfinder 0.1.0

--- views/index/index.js
+++ views/index/index.js
@@ -7,10 +7,10 @@ function getHandler(defaults, view) {
 	return function renderIndex(req, res) {
 		res.render('index', {
 			layout: false,
-			title: 'Webfiller',
+			title: 'Webfiller on Express 2',
 			bindings: {
-				'': {
-					fragment: 'header',
+				'': { // insert at beginning of html document
+					fragment: 'header', // the header fragment
 				},
 				h1: {
 					append: 'title',
@@ -23,16 +23,25 @@ function getHandler(defaults, view) {
 	}
 }
 
+// server fragments
 exports.fragments = {
-	header: {
-		title: 'title',
+	header: { // fragment name 'header', in ./header.html
+		title: 'title',	// for all title elements, insert the title data field
 	}
 }
 
+// front-end and server-side fragments
 exports.publicFragments = {
-	listentry: {
-		li: {
-			myfunction: 5,
-		}
-	}
+	listentry: {	// fragment name listentry, in './listentry.html'
+		li: [	// for all li elements
+			{
+				addClass: 'red'	// add the red class
+			},
+			{
+				myfunction: 5,	// execute the custom 'myfunction'
+				// my function is declared as dual-side JavaScript
+				// in './index_2.js'
+			},
+		],
+	},
 }
