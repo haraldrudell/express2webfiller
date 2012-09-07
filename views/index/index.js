d5487b3adfe4766a28ab2e3b7197352229cff5ac
Harald Rudell <harald@therudells.com>
2012-09-07 07:42:52 -0700

    compatible webfiller 0.1.2

--- views/index/index.js
+++ views/index/index.js
@@ -10,13 +10,13 @@ function getHandler(defaults, view) {
 			title: 'Webfiller on Express 2',
 			bindings: {
 				'': { // insert at beginning of html document
-					fragment: 'header', // the header fragment
+					fragment: ['header'], // the header fragment
 				},
 				h1: {
 					append: 'title',
 				},
 				'#list': {
-					fragment: 'listentry',
+					fragment: ['listentry'],
 				},
 			}
 		})
@@ -35,7 +35,7 @@ exports.publicFragments = {
 	listentry: {	// fragment name listentry, in './listentry.html'
 		li: [	// for all li elements
 			{
-				addClass: 'red'	// add the red class
+				addClass: ['red']	// add the red class
 			},
 			{
 				myfunction: 5,	// execute the custom 'myfunction'
